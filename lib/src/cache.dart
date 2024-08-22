034431a1a37225de5c6e5a957291a6f2ee4efce8
Ryan <wellion@qq.com>
2024-08-22 10:33:29 +0800

    change cache path

--- lib/src/cache.dart
+++ lib/src/cache.dart
@@ -9,7 +9,7 @@ Future<File?> writeToFile(ByteData data,
     {String name = "instrument.sf2"}) async {
   if (kIsWeb) return null;
   final buffer = data.buffer;
-  final directory = await getApplicationDocumentsDirectory();
+  final directory = await getApplicationSupportDirectory();
   final path = "${directory.path}/$name";
   return File(path)
       .writeAsBytes(buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
